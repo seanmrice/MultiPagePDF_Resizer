946d7e6b6529b47770eddebcbaa5c6e2bc6c2831
Sean Rice <687457+seanmrice@users.noreply.github.com>
2022-04-10 18:20:55 -0700

    Update main.py
    LINT + simplified a boolean check

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ temp_dir = "/Windows/Main/PDF Resizer/temp/"
 pdf_file = "Input_PDF_File.pdf"
 
 # We have to open the file prior to entering the loop to avoid a bug with PyPDF2
-pdf_in_file = open(pdf_file,'rb')
+pdf_in_file = open(pdf_file, 'rb')
+
 
 # SizeCheck makes sure we don't process a file that already has the correct dimensions
 # Returns false when a PDF does not need resizing, returns true when it needs to be resized
@@ -24,13 +25,15 @@ def SizeCheck(currentPdfWidth, currentPdfHeight, newHeight, newWidth):
     else:
         return "true"
 
+
 # OrientationCheck returns the orientation of the PDF page (portrait or landscape) as a string
 def OrientationCheck(currentPdfWidth, currentPdfHeight):
     if currentPdfWidth > currentPdfHeight:
         return "landscape"
     else:
         return "portrait"
-    
+
+
 needsResize = "true"
 
 # Userspace units are equal to inch * 72
@@ -51,13 +54,13 @@ pages_no = inputpdf.numPages
 
 # Check if temp dir is actually there, if not, then create it under the working directory
 temp_dir_check = os.path.exists(temp_dir)
-if temp_dir_check == False:
+if not temp_dir_check:
     os.mkdir(working_dir + "temp/")
 
 for i in range(pages_no):
     inputpdf = PyPDF2.PdfFileReader(pdf_in_file)
     currentPage = inputpdf.getPage(i)
-    
+
     pdfMediaBox = currentPage.mediaBox
     currentPdfWidth = pdfMediaBox[2]
     currentPdfHeight = pdfMediaBox[3]
@@ -77,15 +80,17 @@ for i in range(pages_no):
         elif pdf_file.upper().find("LTR") != -1:
             newWidth = LTR_h
             newHeight = LTR_w
-    needsResize = SizeCheck(currentPdfWidth, currentPdfHeight, newHeight, newWidth)            
-    if needsResize == "true": currentPage.scaleTo(newWidth, newHeight)
-    else: break
-    
+    needsResize = SizeCheck(currentPdfWidth, currentPdfHeight, newHeight, newWidth)
+    if needsResize == "true":
+        currentPage.scaleTo(newWidth, newHeight)
+    else:
+        break
+
     output = PyPDF2.PdfFileWriter()
     currentPage.scaleTo(newWidth, newHeight)
     output.addPage(currentPage)
-    
-    # Each page will temporarily be seperately written to a PDF file in the temp directory
+
+    # Each page will temporarily be separately written to a PDF file in the temp directory
     with open(temp_dir + "document-page%s.pdf" % i, "wb") as outputStream:
         output.write(outputStream)
 pdf_in_file.close()
@@ -93,7 +98,7 @@ pdf_in_file.close()
 if needsResize == "true":
     file_list = glob.glob(temp_dir + "*document-page*.pdf")
     file_list.sort()
-    
+
     # Append each merged page to the output document (original filename of the input PDF)
     merger = PdfFileMerger()
     for pdf in file_list:
@@ -106,4 +111,3 @@ if needsResize == "true":
         if os.path.exists(pdf):
             os.remove(pdf)
 print("Processing of file:", pdf_file, "complete.")
-
